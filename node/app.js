3b9bbe1ee2b6fc7a92942d0949d6e8828b1b3323
Rax <damnloardbruh@gmail.com>
2020-04-12 22:25:55 +0700

    first push on server

--- node/app.js
+++ node/app.js
@@ -6,31 +6,33 @@ const getBody = require('./utils/getBody');
 
 
 // config all file need to be run
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 const pDir = path.join(__dirname, '../public');
-const viewDir = path.join(__dirname,'../templates/views');
+const viewDir = path.join(__dirname, '../templates/views');
 app.set('views', viewDir);
 app.use(express.static(pDir));
 
+app.set("port", process.env.PORT || 5000);
 
 
 
 
 app.get('/', (req, res) => {
-    getBody((err,apiDate)=>{
-        if(err){
-            res.send({err});
+    getBody((err, apiDate) => {
+        if (err) {
+            res.send({
+                err
+            });
         }
-        res.send("index",{
+        res.send("index", {
             data_info: apiDate,
         })
     });
-    
+
     res.render("index");
 })
 
 
-
-app.listen('3000', (req, res) => {
-    console.log("running on port " + `http://localhost:3000/`);
-})
+app.listen(app.get("port"), function () {
+    console.log("running on port", app.get("port"));
+});
